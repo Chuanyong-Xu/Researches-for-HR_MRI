1a3a0084e648faa74d4a4a12cf4b2c78429a481a
Chuanyong-Xu <63353359+Chuanyong-Xu@users.noreply.github.com>
2025-11-17 16:15:12 +0800

    Add files via upload

---
+++ MRI_behavioural/Model_fitting_Tom/Hybrid_model/estimating_PE_Hybrid.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 10:01:43 2019
+
+@author: pieter
+"""
+
+import estimation_Hybrid as estimation
+import likelihood_Hybrid as likelihood
+import sim_data_Hybrid as sim_data
+
+import numpy as np
+import pandas as pd
+import os
+
+pplist=[1101, 1104, 1105, 1108, 1110, 1114, 1116, 1117, 1120, 1121, 1123, 1124, 1125, 1126, 1132,
+        1141, 1142, 1144, 1146, 1147, 1148, 1150, 1155, 1157, 1160, 1161, 1162, 1164, 1165]
+
+method="g"
+
+pars=[]
+Lik=[]
+loop=0
+
+os.chdir('/Users/vincentxu/Desktop/Model/IOM-CBM-5points-MRI/MRI_behavioural-29subs/Beha_ModCom/Hybrid_data')
+
+for p in pplist:
+    print("Estimating parameters of subject {}".format(p))
+    
+    file = 'Behavioral_data_subject_{0}_Hybrid.csv'.format(int(p))
+    
+    if method=="gradient":
+        est=estimation.estimate(file)
+        pars.append(est)
+        Lik.append(-est.fun)
+    else:
+        est = estimation.evol_estimate(file)
+        pars.append(est.x)
+        Lik.append(-est.fun)
+        print(est.message)
+        
+    print("Estimated parameters are {}, {} and {}".format(pars[loop][0], pars[loop][1], pars[loop][2]))
+    
+    sim_data.simulate_data(alpha = pars[loop][0], beta = pars[loop][1], decrease=pars[loop][2], file_name = file)
+    
+    print("Estimated the prediction errors" )
+    print("LogLikelihood was {}".format(Lik[loop]))
+    loop+=1
+
+print(pars)
+Learning_rates= [item[0] for item in pars]
+Temperatures= [item[1] for item in pars]
+Decreases= [item[2] for item in pars]
+print(Learning_rates)
+print(Temperatures)
+print(Decreases)
+print("Mean learning rate")
+print(np.mean(Learning_rates))
+print("std learning rate")
+print(np.std(Learning_rates))
+print("Mean temperature")
+print(np.mean(Temperatures))
+print("std temperature")
+print(np.std(Temperatures))
+print("Mean decrease")
+print(np.mean(Decreases))
+print("std decrease")
+print(np.std(Decreases))
+print("Mean LogLikelihood")
+print(np.mean(Lik))
+print("std Loglikelihood")
+print(np.std(Lik))
+AIC=-2*np.asarray(Lik)+ 2*(len(pars[0]))
+print("Mean AIC")
+print(np.mean(AIC))
+print("std AIC")
+print(np.std(AIC))
+
+for p in pplist:
+    file = 'Behavioral_data_subject_{0}_Hybrid.csv'.format(int(p))
+    data = pd.read_csv(file,encoding='utf-8')
+    BIC=np.log(len(data.values[:,1]))*len(pars[0])-2*np.asarray(Lik)
+
+new_filename='Hybrid_output.csv'.format(int(p))
+column_list = ["Subject", "Learning rate", "Temperature", "Decrease", "LogLik", "AIC", "BIC"]
+    
+output=pd.DataFrame({ 'Subject':pplist, 'Learning rate':Learning_rates, 'Temperature':Temperatures, "Decrease": Decreases, 'LogLik':Lik, 'AIC':AIC, 'BIC':BIC}, columns = column_list)
+    
+output.to_csv(new_filename, columns = column_list, float_format ='%.5f')
+
+
---
+++ MRI_behavioural/Model_fitting_Tom/Sync_model/make_data_Sync.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 13:56:14 2019
+
+@author: pieter
+"""
+
+import pandas as pd
+import os
+import numpy as np
+
+pplist=[1101, 1104, 1105, 1108, 1110, 1114, 1116, 1117, 1120, 1121, 1123, 1124, 1125, 1126, 1132,
+        1141, 1142, 1144, 1146, 1147, 1148, 1150, 1155, 1157, 1160, 1161, 1162, 1164, 1165]
+
+column_list = ["Rule", "Stimulus", "Response", "Reward", "Expected value", "PE_estimate","Response_likelihood","Module"]
+
+for p in pplist:
+    os.chdir('/Users/vincentxu/Desktop/Model/IOM-CBM-5points-MRI/MRI_behavioural-29subs/Beha_ModCom/data')
+    
+    filename= "data_29sub_sub{0}.csv".format(int(p))
+
+    data = pd.read_csv(filename,encoding='utf-8')
+    
+    Rule=data.values[:,1]
+    Stim=data.values[:,2]
+    #Resp=(data.values[:,5]=='f').astype(int)
+    Resp=data.values[:,4]
+    Rew=data.values[:,5]
+    Rew[Rew==2]=0
+    
+    new_filename='Behavioral_data_subject_{0}_Sync.csv'.format(int(p))
+    
+    new_data=pd.DataFrame({ 'Rule':Rule, 'Stimulus':Stim, 'Response':Resp, 'Reward':Rew, 'Expected value':np.zeros(len(Rule)), 'PE estimate':np.zeros(len(Rule)), "Response_likelihood":np.zeros(len(Rule)), "Module":np.zeros(len(Rule))}, columns = column_list)
+    
+    os.chdir('/Users/vincentxu/Desktop/Model/IOM-CBM-5points-MRI/MRI_behavioural-29subs/Beha_ModCom/Sync_data')
+    new_data.to_csv(new_filename, columns = column_list, float_format ='%.3f')
+    
+    
